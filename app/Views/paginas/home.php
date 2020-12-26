3338b00bed50fb466bc1aedc8548f6f4d57a3257
Clamant96 <kevin.lazzarotto@hotmail.com>
2020-12-26 20:27:36 -0300

    Responsividade do sistema para as paginas de gerenciamento de perfil e meus produtos

--- app/Views/paginas/home.php
+++ app/Views/paginas/home.php
@@ -13,7 +13,7 @@
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                             </svg>
                         </div>
-                        <p>Gerenciar</p>
+                        <p>Perfil</p>
                     </a>
                 </div>
             </div>
--- app/Views/usuarios/pedidos.php
+++ app/Views/usuarios/pedidos.php
@@ -24,7 +24,7 @@
                     </div>
                     <div id="botoes">
                         <div id="comprar">
-                            <a href="<?= URL.'/usuarios/vizualizarPerfil/71' ?>">
+                            <a href="<?= URL.'/usuarios/vizualizarPerfil/'.$_SESSION['usuario_id'] ?>">
                                 <div id="icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
@@ -37,7 +37,7 @@
                             </a>
                         </div>
                         <div id="comprar">
-                            <a href="<?= URL.'/usuarios/vizualizarPedidos/71' ?>">
+                            <a href="<?= URL.'/usuarios/vizualizarPedidos/'.$_SESSION['usuario_id'] ?>">
                                 <div id="icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
